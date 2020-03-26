f381450986a29e6beb2bc421644c525b8496bbbd
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2020-03-26 11:24:24 +0300

    feat: implement function writeToFile for output

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,6 @@
 const { Command } = require('commander');
+const fs = require('fs');
+const path = require('path');
 
 const program = new Command();
 
@@ -6,13 +8,25 @@ console.log('node run');
 
 const actionFunc = x => console.log(`action: ${x}`);
 
+const writeToFile = (fileName, data) => {
+  const text = data || 'text ttttt2';
+  fs.writeFile(path.join(__dirname, fileName), `${text}`, err => {
+    if (err) {
+      console.log('error: ', err);
+      return null;
+    }
+    return text;
+  });
+};
+
 program
-  .option('-a, --action <actionType>', 'action')
-  .option('-s, --shift <shiftType>', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
+  .option('-s, --shift <shiftType>', 'shift')
+  .option('-i, --input <type>', 'input file')
+  .option('-o, --output <type>', 'output file')
+  .option('-a, --action <actionType>', 'action');
 
 program.parse(process.argv);
 
 if (program.action) actionFunc(program.action);
 if (program.shift) console.log(program.shift);
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
+if (program.output) writeToFile(`${program.output}`);
