fab0d30f57f1006b9bd3ef9a39dee12598dae421
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2020-04-05 17:44:42 +0300

    feat: implement user.getById function

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,11 +1,24 @@
 const users = [
-  { id: 1, name: 'Alex', login: '123sas' },
-  { id: 2, name: 'Nick', login: '456nik' }
+  { id: 1, name: 'Alex', login: 'Alex11', password: 'aall' },
+  { id: 2, name: 'Nick', login: 'Nick22', password: 'nik123' }
 ];
 
 const getAll = async () => {
   // TODO: mock implementation. should be replaced during task development
+  console.log('getAll');
   return users;
 };
 
-module.exports = { getAll };
+const getById = async id => {
+  console.log('getById', id);
+  let user;
+  users.map(item => {
+    if (item.id === +id) {
+      console.log('id', item.id, +id);
+      user = item;
+    }
+  });
+  return user;
+};
+
+module.exports = { getAll, getById };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,15 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
+  console.log('--------/users');
+});
+
+router.route('/:id').get(async (req, res) => {
+  const { id } = req.params;
+  const user = await usersService.getById(id);
+  res.json(User.toResponse(user));
+  console.log('--------users/id--', id, user);
 });
 
 module.exports = router;
