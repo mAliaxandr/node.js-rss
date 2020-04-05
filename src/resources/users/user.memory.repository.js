169777ba3ba374eac4542605b9d5b38f009c6868
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2020-04-05 13:43:58 +0300

    feat: create start userData

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,11 @@
+const users = [
+  { id: 1, name: 'Alex', login: '123sas' },
+  { id: 2, name: 'Nick', login: '456nik' }
+];
+
 const getAll = async () => {
   // TODO: mock implementation. should be replaced during task development
-  return [];
+  return users;
 };
 
 module.exports = { getAll };
