f88e502b3d3d3af051886f606b501db1497ba03b
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2020-04-17 18:03:00 +0300

    feat: implement board model with mongooseSchema

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,11 +1,23 @@
 const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-class Board {
-  constructor({ id = uuid(), title = 'BOARD', columns = [] } = {}) {
-    this.id = id;
-    this.title = title;
-    this.columns = columns;
+const boardSchema = new mongoose.Schema({
+  title: String,
+  columns: Array,
+  _id: {
+    type: String,
+    default: uuid
   }
-}
+});
+
+const Board = mongoose.model('Board', boardSchema);
+
+// class Board {
+//   constructor({ id = uuid(), title = 'BOARD', columns = [] } = {}) {
+//     this.id = id;
+//     this.title = title;
+//     this.columns = columns;
+//   }
+// }
 
 module.exports = Board;
