82bf384f570f74988783ddefa1e643c110fd0f38
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2020-04-17 18:37:07 +0300

    feat: implement function updateBoard with mongo DB

--- src/resources/boards/board.db.repository.js
+++ src/resources/boards/board.db.repository.js
@@ -12,4 +12,12 @@ const getById = async id => {
   return Board.findOne({ _id: id });
 };
 
-module.exports = { createBoard, getAll, getById };
+const updateBoard = async board => {
+  return Board.updateOne({ _id: board.id }, board);
+};
+
+const deleteUser = async id => {
+  return await User.deleteOne({ _id: id });
+};
+
+module.exports = { createBoard, getAll, getById, updateBoard };
