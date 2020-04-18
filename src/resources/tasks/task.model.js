f88bddbf51cfa6588bc87f5976898531295a3f59
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2020-04-18 14:24:38 +0300

    feat: implement taskModel with mongooseScheme

--- src/resources/tasks/task.model.js
+++ src/resources/tasks/task.model.js
@@ -1,23 +1,44 @@
 const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-class Task {
-  constructor({
-    id = uuid(),
-    title = 'TASK',
-    order = '11111',
-    description = 'Task description',
-    userId = null,
-    boardId = null,
-    columnId = null
-  } = {}) {
-    this.id = id;
-    this.title = title;
-    this.order = order;
-    this.description = description;
-    this.userId = userId;
-    this.boardId = boardId;
-    this.columnId = columnId;
+const taskSchema = new mongoose.Schema({
+  title: String,
+  order: String,
+  description: String,
+  userId: String,
+  boardId: String,
+  columnId: String,
+  _id: {
+    type: String,
+    default: uuid
   }
-}
+});
+
+taskSchema.statics.toResponse = task => {
+  const { id, title, order, description, userId, boardId, columnId } = task;
+  return { id, title, order, description, userId, boardId, columnId };
+};
+
+const Task = mongoose.model('Task', taskSchema);
+
+// class Task {
+//   constructor({
+//     id = uuid(),
+//     title = 'TASK',
+//     order = '11111',
+//     description = 'Task description',
+//     userId = null,
+//     boardId = null,
+//     columnId = null
+//   } = {}) {
+//     this.id = id;
+//     this.title = title;
+//     this.order = order;
+//     this.description = description;
+//     this.userId = userId;
+//     this.boardId = boardId;
+//     this.columnId = columnId;
+//   }
+// }
 
 module.exports = Task;
