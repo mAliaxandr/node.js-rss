2265ca361039f31f61fa2509e13df42a6a850443
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2020-04-20 12:55:29 +0300

    refactor: delete coments

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -17,12 +17,4 @@ boardSchema.statics.toResponse = board => {
 
 const Board = mongoose.model('Board', boardSchema);
 
-// class Board {
-//   constructor({ id = uuid(), title = 'BOARD', columns = [] } = {}) {
-//     this.id = id;
-//     this.title = title;
-//     this.columns = columns;
-//   }
-// }
-
 module.exports = Board;
--- src/resources/tasks/task.model.js
+++ src/resources/tasks/task.model.js
@@ -21,24 +21,4 @@ taskSchema.statics.toResponse = task => {
 
 const Task = mongoose.model('Task', taskSchema);
 
-// class Task {
-//   constructor({
-//     id = uuid(),
-//     title = 'TASK',
-//     order = '11111',
-//     description = 'Task description',
-//     userId = null,
-//     boardId = null,
-//     columnId = null
-//   } = {}) {
-//     this.id = id;
-//     this.title = title;
-//     this.order = order;
-//     this.description = description;
-//     this.userId = userId;
-//     this.boardId = boardId;
-//     this.columnId = columnId;
-//   }
-// }
-
 module.exports = Task;
