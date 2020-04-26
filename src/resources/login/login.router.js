998450790caa6bd64169c9a5f3befb082378bddb
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2020-04-26 17:49:36 +0300

    feat: add EroorHandler to loginRouter

--- src/resources/login/login.router.js
+++ src/resources/login/login.router.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const loginService = require('./login.service');
+const { ErrorHandler } = require('../../error/error');
 
 router.route('/').post(async (req, res, next) => {
   try {
     const jwtToken = await loginService.getToken(req.body);
     if (jwtToken) {
       res.status(200).json({ token: jwtToken });
+    } else {
+      throw new ErrorHandler(403, 'Forbidden');
     }
     console.log('loginRouter -- token---- ', jwtToken);
   } catch (error) {
